cf662e6a0c88935c4ca6ebcf4f3b3a238b458c56
Кашенцев Ростислав ИКБО-06-21 <kashencevrostik@gmail.com>
2022-09-24 16:48:39 +0300

    Added home screen

--- app/src/main/java/com/example/thewitcherwildhuntguidbook/MainActivity.java
+++ app/src/main/java/com/example/thewitcherwildhuntguidbook/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_map,
                 R.id.navigation_story,
+                R.id.navigation_home,
                 R.id.navigation_items,
                 R.id.navigation_builds)
                 .build();
