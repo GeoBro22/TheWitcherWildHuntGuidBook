574cf0ebc9d7277c2cee10b33345b5b4d014712e
Кашенцев Ростислав ИКБО-06-21 <kashencevrostik@gmail.com>
2022-10-06 10:56:28 +0300

    Было создано начальное взаимодействие с картой.

--- app/src/main/java/com/example/thewitcherwildhuntguidbook/MainActivity.java
+++ app/src/main/java/com/example/thewitcherwildhuntguidbook/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.thewitcherwildhuntguidbook;
 
+import android.app.Fragment;
 import android.os.Bundle;
+import android.view.MotionEvent;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
@@ -24,8 +26,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_map,
                 R.id.navigation_story,
--- app/src/main/java/com/example/thewitcherwildhuntguidbook/ui/Map/Map.java
+++ app/src/main/java/com/example/thewitcherwildhuntguidbook/ui/Map/Map.java
@@ -1,66 +1,110 @@
 package com.example.thewitcherwildhuntguidbook.ui.Map;
 
+import android.graphics.Point;
+import android.graphics.PointF;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.ScaleGestureDetector;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
 
 import com.example.thewitcherwildhuntguidbook.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link Map#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class Map extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    // The position from which the swipe starts
+    private final PointF startPosition = new PointF(0.0F, 0.0F);
+    // Current position when moving
+    private final PointF currentPosition = new PointF(0.0F, 0.0F);
+    // Difference between the current swipe position and the previous one
+    private Point screenSize = new Point(0,0);
+    private final PointF offset = new PointF(0.0F, 0.0F);
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
 
-    public Map() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment Map.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static Map newInstance(String param1, String param2) {
-        Map fragment = new Map();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    //Class for scale processing
+    class ScaleListener extends ScaleGestureDetector.SimpleOnScaleGestureListener {
+        @Override
+        public boolean onScale(ScaleGestureDetector detector) {
+            FACTOR *= detector.getScaleFactor();
+            FACTOR = Math.max(0.8f, Math.min(FACTOR, 3f));
+            imageView.setScaleY(FACTOR);
+            imageView.setScaleX(FACTOR);
+            return true;
         }
     }
+
+    private ScaleGestureDetector scaleGestureDetector;
+    //Scaling size
+    private float FACTOR = 1.0f;
+    //Image of map
+    ImageView imageView;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_map, container, false);
     }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        imageView = (ImageView) view.findViewById(R.id.image_map_velen);
+        scaleGestureDetector = new ScaleGestureDetector(view.getContext(), new ScaleListener());
+        scaleGestureDetector.setQuickScaleEnabled(false);
+        screenSize.set(
+                view.getResources().getDisplayMetrics().widthPixels / 2,
+                (view.getResources().getDisplayMetrics().heightPixels / 2) - 100);
+        view.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                scaleGestureDetector.onTouchEvent(motionEvent);
+                scrollImageView(motionEvent);
+                switch (motionEvent.getAction()) {
+                    case MotionEvent.ACTION_UP:
+                        if(FACTOR <= 1f) {
+                            FACTOR = 1f;
+                            imageView.animate().scaleY(1f).setDuration(500);
+                            imageView.animate().scaleX(1f).setDuration(500);
+                            imageView.animate().x(0).setDuration(500);
+                            imageView.animate().y(0).setDuration(500);
+                        }
+                        break;
+                }
+                return true;
+            }
+        });
+    }
+
+    protected void scrollImageView(MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                startPosition.set(motionEvent.getRawX(), motionEvent.getRawY());
+                currentPosition.set(startPosition);
+                break;
+            case MotionEvent.ACTION_MOVE:
+                offset.set(motionEvent.getRawX() - currentPosition.x, motionEvent.getRawY() - currentPosition.y);
+                if (imageView.getY() < -screenSize.y*(FACTOR / 1.5f) && offset.y < 0)
+                    offset.y = 0;
+                if (imageView.getX() < -screenSize.x*(FACTOR / 1.5f) && offset.x < 0)
+                    offset.x = 0;
+                if (imageView.getY() > screenSize.y*(FACTOR / 1.5f) && offset.y > 0)
+                    offset.y = 0;
+                if (imageView.getX() > screenSize.x*(FACTOR / 1.5f) && offset.x > 0)
+                    offset.x = 0;
+                imageView.setX(imageView.getX() + offset.x);
+                imageView.setY(imageView.getY() + offset.y);
+                currentPosition.set(motionEvent.getRawX(), motionEvent.getRawY());
+                break;
+        }
+    }
+
 }
