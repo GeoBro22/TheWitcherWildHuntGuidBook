cffbbedd1dc2d2846bda8f217778afdf726716b2
viktorger <viktorger288@gmail.com>
2022-10-13 05:24:02 +0300

    Added empty adapter for RecyclerView in Items Fragment, storage class - Item

---
+++ app/src/main/java/com/example/thewitcherwildhuntguidbook/ui/Items/Item.java
@@ -0,0 +1,5 @@
+package com.example.thewitcherwildhuntguidbook.ui.Items;
+
+public class Item {
+
+}
--- app/src/main/java/com/example/thewitcherwildhuntguidbook/ui/Items/Items.java
+++ app/src/main/java/com/example/thewitcherwildhuntguidbook/ui/Items/Items.java
@@ -2,7 +2,10 @@ package com.example.thewitcherwildhuntguidbook.ui.Items;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,9 +15,16 @@ import com.example.thewitcherwildhuntguidbook.R;
 
 public class Items extends Fragment {
 
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_items, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
     }
 }
