1de95aa50711d79eb15d9a6bd7f10436e35e5da3
viktorger <viktorger288@gmail.com>
2022-10-13 05:39:46 +0300

    FragmentItemsBinding initializing

--- app/src/main/java/com/example/thewitcherwildhuntguidbook/ui/Items/Items.java
+++ app/src/main/java/com/example/thewitcherwildhuntguidbook/ui/Items/Items.java
@@ -5,26 +5,28 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.example.thewitcherwildhuntguidbook.R;
+import com.example.thewitcherwildhuntguidbook.databinding.FragmentItemsBinding;
 
 public class Items extends Fragment {
-
+    FragmentItemsBinding binding;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_items, container, false);
+        binding = FragmentItemsBinding.inflate(inflater,container, false);
+        return binding.getRoot();
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
+        ItemAdapter itemAdapter = new ItemAdapter();
+        binding.recyclerView.setAdapter(itemAdapter);
     }
 }
