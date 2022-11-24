5c8d08c32e39f6cafbdf6a00fe1b26fbf8cd153a
GeoBro22 <georgejiadze@gmail.com>
2022-11-24 17:01:02 +0300

    wolf's set

--- app/src/main/java/com/example/thewitcherwildhuntguidbook/ui/Build/Builds.java
+++ app/src/main/java/com/example/thewitcherwildhuntguidbook/ui/Build/Builds.java
@@ -5,6 +5,7 @@ import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import android.view.MotionEvent;
 import android.view.LayoutInflater;
@@ -13,8 +14,11 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.thewitcherwildhuntguidbook.R;
 import com.example.thewitcherwildhuntguidbook.databinding.FragmentBuildsBinding;
@@ -24,7 +28,7 @@ import java.util.Objects;
 public class Builds extends Fragment {
     FragmentBuildsBinding binding;
 
-    String[] countries = { "Школа Волка", "Школа Кота", "Школа Медведя", "Школа Мантикоры", "Школа Змеи"};
+    String[] schools = { "Школа Волка", "Школа Кота", "Школа Медведя", "Школа Мантикоры", "Школа Змеи"};
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -35,38 +39,29 @@ public class Builds extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        TextView selection = (TextView) view.findViewById(R.id.selection);
-        //android:text="@string/hello";
-        // Инициализируем компонент
-        //TextView textView = (TextView) view.findViewById(R.id.textView);
-// задаём текст
-        //binding.textView.setText("Hello Kitty!");
-// или с использованием текстовых ресурсов
-        //textView.setText(R.string.hello);
-
         Spinner spinner = view.findViewById(R.id.spinner);
-        // Создаем адаптер ArrayAdapter с помощью массива строк и стандартной разметки элемета spinner
-        ArrayAdapter<String> adapter = new ArrayAdapter(view.getContext(), android.R.layout.simple_spinner_item, countries);
-        // Определяем разметку для использования при выборе элемента
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Применяем адаптер к элементу spinner
+        ArrayAdapter<String> adapter = new ArrayAdapter(getActivity(), R.layout.custom_spinner, schools);
+        adapter.setDropDownViewResource(R.layout.item_spinner);
         spinner.setAdapter(adapter);
+        ConstraintLayout wolf = view.findViewById(R.id.wolf);
+        //layout.setVisibility(View.INVISIBLE);
+        //view(scrollView);
         AdapterView.OnItemSelectedListener itemSelectedListener = new AdapterView.OnItemSelectedListener() {
+
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
                 // Получаем выбранный объект
                 String item = (String)parent.getItemAtPosition(position);
-                if (item.equals("Школа Волка"))
-                    selection.setText(item);
-                else if (item.equals("Школа Кота"))
-                    selection.setText(item);
-                else if (item.equals("Школа Медведя"))
-                    selection.setText(item);
-                else if (item.equals("Школа Мантикоры"))
-                    selection.setText(item);
-                else if (item.equals("Школа Змеи"))
-                    selection.setText(item);
+                //if (item.equals("Школа Волка"))
+                  //  selection.setText(item);
+                //else if (item.equals("Школа Кота"))
+                  //  selection.setText(item);
+                //else if (item.equals("Школа Медведя"))
+                  //  selection.setText(item);
+                //else if (item.equals("Школа Мантикоры"))
+                  //  selection.setText(item);
+                //else if (item.equals("Школа Змеи"))
+                  //  selection.setText(item);
             }
 
             @Override
