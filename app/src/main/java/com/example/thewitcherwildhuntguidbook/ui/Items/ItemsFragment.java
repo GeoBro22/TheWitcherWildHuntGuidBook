8ebb66a62f655d6c53ad13515d355eb700721558
viktorger <viktorger288@gmail.com>
2022-11-30 00:07:20 +0300

    RecyclerView rebuild with GridLayoutManager

--- app/src/main/java/com/example/thewitcherwildhuntguidbook/ui/Items/ItemsFragment.java
+++ app/src/main/java/com/example/thewitcherwildhuntguidbook/ui/Items/ItemsFragment.java
@@ -10,13 +10,16 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.thewitcherwildhuntguidbook.R;
 import com.example.thewitcherwildhuntguidbook.databinding.FragmentItemsBinding;
 
 import java.util.ArrayList;
 
-public class Items extends Fragment {
+public class ItemsFragment extends Fragment {
     FragmentItemsBinding binding;
     ArrayList<Item> items;
+
+
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container,
@@ -29,10 +32,14 @@ public class Items extends Fragment {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         items = new ArrayList<Item>();
+        initArray();
+        ItemAdapter itemAdapter = new ItemAdapter(items);
 
-        ItemAdapter itemAdapter = new ItemAdapter(getContext() ,items);
-        //binding.recyclerView.setAdapter(itemAdapter);
+        binding.recyclerView.setAdapter(itemAdapter);
     }
 
-    // TODO: items initializer
+    void initArray() {
+        items.add(new Item(R.drawable.silver_sword,"An'ferthe","Magic", "1.81"));
+        items.add(new Item(R.drawable.silver_sword,"An'ferthe","Magic", "1.81"));
+    }
 }
